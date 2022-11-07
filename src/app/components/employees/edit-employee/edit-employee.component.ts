688229695329b07e2521d501d922532621888044
ap-stein <a_stein2001@yahoo.com>
2022-11-07 06:55:32 -0600

    Added update and delete.  All complete

--- src/app/components/employees/edit-employee/edit-employee.component.ts
+++ src/app/components/employees/edit-employee/edit-employee.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Employee } from 'src/app/models/employee.model';
 import { EmployeesService } from 'src/app/services/employees.service';
 
@@ -18,7 +18,7 @@ export class EditEmployeeComponent implements OnInit {
     salary: 0,
     department: ''
   }
-  constructor(private route: ActivatedRoute, private employeeService: EmployeesService) { }
+  constructor(private route: ActivatedRoute, private employeeService: EmployeesService, private router: Router) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe({
@@ -37,4 +37,21 @@ export class EditEmployeeComponent implements OnInit {
     })
   }
 
+  updateEmployee() {
+    this.employeeService.updateEmployee(this.employeeDetails.id, this.employeeDetails)
+    .subscribe({
+      next: (response) => {
+        this.router.navigate(['employees']);
+      }
+    })
+  }
+
+  deleteEmployee(id: string) {
+    this.employeeService.deleteEmployee(id)
+    .subscribe({
+      next: (response) => {
+        this.router.navigate(['employees']);
+      }
+    })
+  }
 }
